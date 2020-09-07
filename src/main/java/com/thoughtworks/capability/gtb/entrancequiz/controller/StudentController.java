726c254003269e7521f5f44a8c42c609d1a340a6
RuilingGuo <ruiling.guo1@thoughtworks.com>
2020-09-07 16:09:54 +0800

    [ruiling] feat: add init student list

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.thoughtworks.capability.gtb.entrancequiz.pojo.Student;
 import com.thoughtworks.capability.gtb.entrancequiz.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/student",produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
 public class StudentController {
 
     @Autowired
--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/repository/StudentRepository.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/repository/StudentRepository.java
@@ -11,23 +11,59 @@ import java.util.List;
 @Repository
 public class StudentRepository {
     private List<Student> studentList;
+    private Integer studentIncId;
 
 
-    public StudentRepository(){
+    public StudentRepository() {
         this.studentList = new ArrayList<>();
-
-        initStudentList(this.studentList);
-    }
-
-    private void initStudentList(List<Student> studentList) {
-        studentList.add(Student.builder()
-                .id(1)
-                .name("student")
-                .build());
+        this.studentIncId = 1;
+        initStudentList();
     }
 
     public List<Student> findAllStudents() {
         return this.studentList;
+    }
+
+    public void addNewStudent(String studentName) {
+        this.studentList.add(new Student(studentIncId++, studentName));
+    }
+
+    private void initStudentList() {
+        addNewStudent("沈乐棋");
+        addNewStudent("徐慧慧");
+        addNewStudent("陈思聪");
+        addNewStudent("王江林");
+        addNewStudent("王登宇");
+        addNewStudent("杨思雨");
+        addNewStudent("江雨舟");
+        addNewStudent("廖燊");
+        addNewStudent("胡晓");
+        addNewStudent("但杰");
+        addNewStudent("盖迈达");
+        addNewStudent("肖美琦");
+        addNewStudent("黄云洁");
+        addNewStudent("齐瑾浩");
+        addNewStudent("刘亮亮");
+        addNewStudent("肖逸凡");
+        addNewStudent("王作文");
+        addNewStudent("郭瑞凌");
+        addNewStudent("李明豪");
+        addNewStudent("党泽");
+        addNewStudent("肖伊佐");
+        addNewStudent("贠晨曦");
+        addNewStudent("李康宁");
+        addNewStudent("马庆");
+        addNewStudent("商婕");
+        addNewStudent("余榕");
+        addNewStudent("谌哲");
+        addNewStudent("董翔锐");
+        addNewStudent("陈泰宇");
+        addNewStudent("赵允齐");
+        addNewStudent("张柯");
+        addNewStudent("廖文强");
+        addNewStudent("刘轲");
+        addNewStudent("廖浚斌");
+        addNewStudent("凌凤仪");
     }
 }
 
